beb517bed694dc65b845c1ff83f9f666e23eeb6b
dagochen <mx.dago@gmx.de>
2015-06-06 19:19:45 +0200

    Ruffix für Schattenhammertexte

---
+++ sql/Update/bugtrackerfix_766.sql
@@ -0,0 +1,3 @@
+-- nur noch 100 Ruf für 10 texte (http://wowdata.buffed.de/item/Quest/Verschluesselter-Schattenhammertext-20404#comments)
+UPDATE `quest_template` SET `RewRepValue1` = 100 WHERE `entry` = 8319 ;
+
