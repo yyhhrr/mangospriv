eb916afbdc3080d897ea2c4c2ffebe26b6dd04fc
dagochen <mx.dago@gmx.de>
2015-06-13 16:13:02 +0200

    Baron Geddon Priority fix

---
+++ sql/Update/geddon_inferno_priofix.sql
@@ -0,0 +1,4 @@
+-- Baron Geddon Inferno Prio Fix
+
+INSERT INTO `mangos_nef`.`spell_priority` (`spell_ID`, `priority`, `spellname`, `effect_description`)
+VALUES (19695, 0, 'Baron Geddon - Inferno', 'AoE');
